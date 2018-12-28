cc1d57f20397d2f0813db0085ffbfaffeefff28b
Bacaluei <13205988366@163.com>
2018-12-28 08:15:03 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>@yield('title', 'Weibo App') —— Bacaluei入门学习</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
